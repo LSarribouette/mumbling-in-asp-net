3fc5e71f6ae8b20141159898a20fd6b56478828a
Lauranne Sarribouette <lauranne.sarribouette@gmail.com>
2023-04-26 17:43:31 +0200

    Ajout d'une API JSON sur Samourai

---
+++ Dojo.Web/ApiControllers/BaseApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dojo.Web.ApiControllers
+{
+    //[Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BaseApiController : ControllerBase
+    {
+    }
+}
---
+++ Dojo.Web/ApiControllers/SamouraiApiController.cs
@@ -0,0 +1,30 @@
+﻿using Dojo.Domain.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Drawing.Printing;
+using System.Drawing.Text;
+
+namespace Dojo.Web.ApiControllers
+{
+    public class SamouraiApiController : BaseApiController
+    {
+        private readonly SamouraiService _service;
+
+        public SamouraiApiController(SamouraiService samouraiService)
+        {
+            _service = samouraiService;
+        }
+
+        [HttpGet("/Api/Samourais")]
+        public IActionResult GetAll()
+        {
+            return Ok(_service.FetchAll());
+        }
+
+        [HttpGet("/Api/Paged")]
+        public IActionResult GetAllPaged(int pageNumber, int pageSize)
+        {
+            return Ok(_service.FetchAllPaged(pageNumber, pageSize));
+        }
+    }
+}
